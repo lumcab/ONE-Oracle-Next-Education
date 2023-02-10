e5f4aeb54c86a0f548569278def82dfdd3475b43
lumcab <lumcab@gmail.com>
2023-02-10 12:56:39 -0500

    Configuracion 10/02/23

---
+++ Java/ProyectByteBank/src/Cuenta.java
@@ -0,0 +1,7 @@
+public class Cuenta {
+    double saldo;
+    int agencia;
+    int numero;
+    String Titular;
+
+}
---
+++ Java/StudioJava/src/TipoVariable.java
@@ -0,0 +1,6 @@
+public class TipoVariable {
+    public void mostrarEdad(int edad){
+
+        System.out.println("la edad es "+ edad);
+    }
+}
